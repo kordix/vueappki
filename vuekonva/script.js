fb16bba20656f78bce7d57eeec78d78de3306cf3
Kordian <kordian.bober@wp.pl>
2019-07-17 16:11:16 +0200

    rysowanie

--- vuekonva/script.js
+++ vuekonva/script.js
@@ -4,8 +4,8 @@ new Vue({
   el:'#app',
   data:{
     configKonva:{
-      width:300,
-      height:500
+      width:400,
+      height:600
     },
     configCircle:{
       radius:30,
@@ -13,35 +13,54 @@ new Vue({
       x:50,
       y:50
     },
-    okleinaConf:{
+    okleinaConf2:{
       image:null,
       x:0,
       height:2056*skala,
       width:1025*skala
     },
-    tloczenieConf:{
+    okleinaConf:{
+      image:null,
+      x:0,
+      height:1186,
+      width:600
+    },
+    tloczenieConf2:{
       image:null,
       x:(1025*skala)/2,
       y:271*skala,
       height:1465*skala,
       width:610*skala,
       offsetX:(610*skala)/2
-
+    },
+    tloczenieConf:{
+      image:null,
+      x:-20,
+      y:0,
+      height:1186, //1186
+      width:600,
+      offsetX:0
     },
     tlo:'01',
+    gco:''
+
+  },
+  created:function(){
+    this.drawokleina(),
+    this.drawtloczenie()
   },
   methods:{
     drawokleina:function(){
       const okleinaObj = new window.Image();
       //http://konfigurator.kordi.com.pl/images/podglad/tla/${this.tlo}.png
-      okleinaObj.src = `${this.tlo}.png`;
+      okleinaObj.src = `okleiny/${this.tlo}.jpg`;
       okleinaObj.onload = () => {
         this.okleinaConf.image = okleinaObj;
       };
     },
     drawtloczenie:function(){
       const tloczenieObj = new window.Image();
-      tloczenieObj.src = '13.png';
+      tloczenieObj.src = 'tloczenia/test-8.png';
       tloczenieObj.onload = () => {
         this.tloczenieConf.image = tloczenieObj;
       }
